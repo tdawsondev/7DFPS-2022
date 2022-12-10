78f715b586d76a25a20dcc232a365ebcc72f023b
jpdaws2003 <jdaws2003@gmail.com>
2022-12-10 16:58:56 -0600

    test commit

--- 7DFPS-2022/Assets/Scripts/Player/PlayerShooting.cs
+++ 7DFPS-2022/Assets/Scripts/Player/PlayerShooting.cs
@@ -8,7 +8,7 @@ public class PlayerShooting : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        int shoot;
     }
 
     // Update is called once per frame
